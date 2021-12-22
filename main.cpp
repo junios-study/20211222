f5846ce5c04940d7e69dddbb3fa430247e33ab80
junios <junios@naver.com>
2021-12-22 15:38:30 +0900

    삼각형 넓이 구하기

--- main.cpp
+++ main.cpp
@@ -6,5 +6,18 @@ int main()
 {
 	cout << "Hello World" << endl;
 
+	int Height = 0;
+	int Base = 0;
+
+	cin >> Height;
+	cin >> Base;
+
+	float Area = 0.0f;
+
+	Area = (static_cast<float>(Height) *
+		static_cast<float>(Base)) / 2.0f;
+
+	cout << Area << endl;
+
 	return 0;
 }
